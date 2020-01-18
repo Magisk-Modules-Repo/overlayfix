f628dcb6895468e8aeccf916f29373e3498eb886
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-01-17 20:02:04 -0500

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,2 @@
-rm -f $NVBASE/service.d/overlay_fix.sh
+rm -f $NVBASE/service.d/overlay_fix.sh $NVBASE/service.d/india_fix.sh
 install -m 0755 $MODPATH/overlay_fix.sh $NVBASE/service.d/overlay_fix.sh
-rm -f $NVBASE/service.d/india_fix.sh
