ea9075ea0ec59785feb0e684010921508b0cb926
Zackptg5 <5107713+Zackptg5@users.noreply.github.com>
2020-01-27 11:54:27 -0500

    update

--- customize.sh
+++ customize.sh
@@ -1,6 +1,8 @@
 rm -f $NVBASE/service.d/overlay_fix.sh $NVBASE/service.d/india_fix.sh
 if [ "`cat /proc/mounts | grep "^overlay "`" ] && $BOOTMODE; then
+  sed -i -e "1aNVBASE=$NVBASE" -e "1aMODPATH=\$NVBASE/modules/$MODID" $MODPATH/overlay_fix.sh
   install -m 0755 $MODPATH/overlay_fix.sh $NVBASE/service.d/overlay_fix.sh
+  touch $MODPATH/skip_mount
 else
   rm -rf $MODPATH 2>/dev/null
   $BOOTMODE || abort "Only flashing in magisk manager supported. Aborting!"
--- overlay_fix.sh
+++ overlay_fix.sh
@@ -1,14 +1,16 @@
 #!/system/bin/sh
-[ -d /data/adb/modules/overlayfix ] || { rm -f $0; exit 0; }
+[ -d $MODPATH ] || { rm -f $0; exit 0; }
 (
 # Wait till boot is completed - overlays are mounted at this point
 while [ $(getprop sys.boot_completed) -ne 1 ]; do
   sleep 1
 done
 # Iterate through each overlay mounted after magisk mount
+rm -rf $MODPATH/.upper $MODPATH/.work
 for i in $(cat /proc/mounts | tac | sed -n '\|/sbin/.magisk/block|q;p' | tac | grep "^overlay " | awk '{print $2}'); do
+  unset REPLACE
   # Get mount arguments
-  FLAGS="$(cat /proc/mounts | grep -E "^overlay .*$i" | awk '{print $4}' | sed 's|lowerdir.*||')"
+  FLAGS="$(cat /proc/mounts | grep -E "^overlay .*$i" | awk '{print $4}' | sed -e 's|lowerdir.*||' -e 's|ro,|rw,|')"
   DIRS="$(cat /proc/mounts | grep -E "^overlay .*$i" | awk '{print $4}' | sed 's|.*lowerdir=||')"
   # Unmount existing overlay
   umount -l $i
@@ -17,10 +19,21 @@ for i in $(cat /proc/mounts | tac | sed -n '\|/sbin/.magisk/block|q;p' | tac | g
     umount -l $j
   done
   # Get list of magisk modules to mount
-  for j in /data/adb/modules/*; do
-    [ -d $j$i ] && DIRS="$j$i:$DIRS"
+  for j in $(find $NVBASE/modules/*$i -maxdepth 0 -type d 2>/dev/null); do
+    DIRS="$j:$DIRS"
+    for k in $(find $j -type f -name '.replace' 2>/dev/null); do
+      REPLACE="$(dirname $k) $REPLACE"
+    done
   done
-  # Remount overlay with all magisk modules included
-  mount -t overlay -o $FLAGS\lowerdir=$DIRS overlay $i
+  # Remount overlay as rw with all magisk modules included
+  mkdir -p $MODPATH/.upper$i $MODPATH/.work$i
+  mount -t overlay -o $FLAGS\lowerdir=$DIRS,upperdir=$MODPATH/.upper$i,workdir=$MODPATH/.work$i overlay $i
+  # Process magisk replacements since it won't work through magisk
+  for j in $REPLACE; do
+    DEST="/$(echo $REPLACE | cut -d / -f6-)"
+    rm -rf $DEST
+    cp -rf $REPLACE $DEST
+  done
+  mount -o remount,ro $i
 done
 )&
